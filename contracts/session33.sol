f9f02d5f31ff3ede97ff7d14dec10e34d3784c52
AditiBajpai22 <bajpaiaditi2002@gmail.com>
2023-06-18 23:02:42 +0530

    initial commit

---
+++ contracts/session33.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Money{
+
+    //Third Account I am considering here
+
+    address ishu=0x617F2E2fD72FD9D5503197092aC168c91465E7f2;
+
+    function getMoney() public payable {} // Temporary Work
+
+    function transferMoney() public{
+
+        payable(ishu).transfer(address(this).balance);
+
+    }
+
+}
